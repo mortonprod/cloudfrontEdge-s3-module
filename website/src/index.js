af7f1c5a6627924b58abb86a7d9502bbb63cb8d2
Alexander Morton <mortonprod@gmail.com>
2019-01-18 17:57:20 +0000

    Add: scss. Update: Configs into dev and production

--- website/src/index.js
+++ website/src/index.js
@@ -1,3 +1,4 @@
+require('./index.scss');
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
--- website/webpack.config.js
+++ website/webpack.config.js
@@ -3,14 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     app: './src/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Production',
       template: 'src/index.html'
     }),
   ],
@@ -18,5 +18,15 @@ module.exports = {
     filename: '[name].bundle.js',
     // path: path.resolve(__dirname)
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+    }]
+}
 };
