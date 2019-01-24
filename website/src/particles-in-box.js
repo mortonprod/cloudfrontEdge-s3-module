bb6176c78facb5be935e6b2249605d3125eb691b
Alexander Morton <mortonprod@gmail.com>
2019-01-24 14:53:06 +0000

    Refactor: Split generation and render into different files

--- website/src/particles-in-box.js
+++ website/src/particles-in-box.js
@@ -1,126 +1,15 @@
-const THREE = require('three');
-const utils = require('./utils');
-const get = require('lodash.get');
-const set = require('lodash.set');
-const OrbitControls = require('three-orbit-controls')(THREE)
+const Utils = require('./utils');
 const variables = require('./variables');
 
-// Create canvas scene
-var renderer = new THREE.WebGLRenderer();
-container = document.getElementById('canvas');
-container.appendChild(renderer.domElement);
 console.debug(`NODE_ENV: ${process.env.NODE_ENV}`);
-const WIDTH = container.clientWidth;
-const HEIGHT = container.clientHeight;
-console.debug(`WIDTH/HEIGHT: ${WIDTH}/${HEIGHT}`);
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(variables.camera.fov, WIDTH / HEIGHT, variables.camera.near, variables.camera.far);
-const depth = variables.camera.initial.position.z;
-const boxWidth = utils.visibleWidthAtZDepth(depth, camera);
-const boxHeight = utils.visibleHeightAtZDepth(depth, camera);
-console.debug(`Width/Height: ${boxWidth}/${boxHeight} at visible at depth ${depth}`);
-renderer.setSize(WIDTH, HEIGHT);
-camera.position.z = depth;
-controls = new OrbitControls(camera);
-controls.target.set(0, 0, 0)
 
-const properties = utils.getProperties(variables, boxWidth, boxHeight);
-const indexToSphereMeshs = new Map();
-console.debug(`variables: ${JSON.stringify(variables)} properties: ${JSON.stringify([...properties])}`);
-createSpheres();
-
-function createSpheres() {
-  for (let i = 0; i < variables.spheres.number; i++) {
-    const colors = variables.spheres.colors;
-    var color = colors[Math.floor(Math.random() * colors.length)];
-    // const color = getSafe(i, 'color');
-    // const color = properties.has(i) && properties.get(i).color ? properties.get(i).color : info.initial.color
-    const material = new THREE.MeshBasicMaterial({
-      color: color
-    });
-    const sphereGeometry = new THREE.SphereGeometry(
-      variables.spheres.initial.radius,
-      variables.spheres.initial.widthSegments,
-      variables.spheres.initial.heightSegments,
-      variables.spheres.initial.phiStart,
-      variables.spheres.initial.phiLength,
-      variables.spheres.initial.thetaStart,
-      variables.spheres.initial.thetaLength
-    );
-    const sphereMesh = new THREE.Mesh(sphereGeometry, material)
-    // setRandomPosition(sphereMesh);
-
-
-    // sphereMesh.position.set(getRandom(),getRandom(),getRandom());
-    // console.debug(sphereMesh.getWorldPosition());
-    scene.add(sphereMesh);
-    indexToSphereMeshs.set(i, sphereMesh);
-    indexToSphereMeshs.get(i).position.set(getSafe(i, 'position.x'), getSafe(i, 'position.y'), getSafe(i, 'position.z'));
-  }
-  // sphereMeshs.get(1).position.set(0, 20, 0);
-  // sphereMeshs.get(2).position.set(0, 0, 20);
-  // return indexToSphereMeshs;
-}
-
+//Initialize
+const utils = Utils(variables);
 var animate = function () {
   requestAnimationFrame(animate);
-  particleSimulation();
-  // spheres[0].rotation.x += 0.01;
-  // spheres[0].rotation.y += 0.01;
+  utils.update();
   
-  renderer.render(scene, camera);
+  utils.renderer.render(utils.scene, utils.camera);
 };
 
 animate();
-
-function particleSimulation() {
-  for (let key of indexToSphereMeshs.keys()) {
-    const mesh = indexToSphereMeshs.get(key);
-    updateWall(key,mesh);
-    updateVelocity(key,mesh);
-  }
-}
-
-// function setRandomPosition(mesh) {
-//   mesh.position.set(getRandom()/2, getRandom()/2, getRandom()/2);
-// }
-
-function updateVelocity(key,mesh) {
-    const x = mesh.position.x + getSafe(key, 'velocity.x');
-    const y = mesh.position.y + getSafe(key, 'velocity.y');
-    const z = mesh.position.z + getSafe(key, 'velocity.z');
-    mesh.position.set(x, y, z);
-}
-
-function updateWall(key,mesh) {
-  let vx = getSafe(key, 'velocity.x');
-  let vy = getSafe(key, 'velocity.y');
-  let vz = getSafe(key, 'velocity.z');
-  if (Math.abs(mesh.position.x) >= variables.box.widthFactor*boxWidth) {
-    vx = -1*vx
-    setSafe(key,'velocity.x',vx);
-  }
-  if (Math.abs(mesh.position.y) >= variables.box.heightFactor*boxHeight) {
-    vy = -1*vy
-    setSafe(key,'velocity.y',vy);
-  }
-  if (Math.abs(mesh.position.z) >= variables.box.depth) {
-    vz = -1*vz
-    setSafe(key,'velocity.z',vz);
-  }
-}
-function getSafe(key,prop) {
-  return get(properties.get(key), prop, get(variables.spheres.initial, prop));
-  // const useProperties = properties.has(i) && (typeof get(properties.get(i), prop) !== 'undefined');
-  // console.debug(`Property found: ${useProperties} with value: ${get(properties.get(i), prop)}`);
-  // const temp =  useProperties ? get(properties.get(i), prop) : get(variables.spheres.initial, prop);
-  // console.debug(`From properties or initial defaults: ${temp}`);
-  // if (typeof temp === 'undefined') {
-  //   throw Error('Missing Info');
-  // }
-  // return temp;
-}
-
-function setSafe(key, prop, value) {
-  set(properties.get(key), prop, value);
-}
--- website/src/utils.js
+++ website/src/utils.js
@@ -1,43 +1,150 @@
+const THREE = require('three');
+const OrbitControls = require('three-orbit-controls')(THREE)
+const get = require('lodash.get');
 const set = require('lodash.set');
-const visibleHeightAtZDepth = ( depth, camera ) => {
+
+const ID = 'canvas';
+
+const visibleHeightAtZDepth = (depth, camera) => {
   // compensate for cameras not positioned at z=0
   const cameraOffset = camera.position.z;
-  if ( depth < cameraOffset ) depth -= cameraOffset;
+  if (depth < cameraOffset) depth -= cameraOffset;
   else depth += cameraOffset;
 
   // vertical fov in radians
-  const vFOV = camera.fov * Math.PI / 180; 
+  const vFOV = camera.fov * Math.PI / 180;
 
   // Math.abs to ensure the result is always positive
-  return 2 * Math.tan( vFOV / 2 ) * Math.abs( depth );
+  return 2 * Math.tan(vFOV / 2) * Math.abs(depth);
 };
 
-const visibleWidthAtZDepth = ( depth, camera ) => {
-  const height = visibleHeightAtZDepth( depth, camera );
+const visibleWidthAtZDepth = (depth, camera) => {
+  const height = visibleHeightAtZDepth(depth, camera);
   return height * camera.aspect;
 };
 
-const getProperties = (variables, boxWidth, boxHeight) => {
-  const properties = new Map();
-  // for(let key of properties.keys()){
-  for(let key=0; key < variables.spheres.number; key++){
-    if(!properties.has(key)) {
-      properties.set(key, {});
+module.exports = function utils(variables) {
+  // Create canvas element and attach to dom
+  var renderer = new THREE.WebGLRenderer();
+  container = document.getElementById(ID);
+  container.appendChild(renderer.domElement);
+  const WIDTH = container.clientWidth;
+  const HEIGHT = container.clientHeight;
+  var camera = new THREE.PerspectiveCamera(variables.camera.fov, WIDTH / HEIGHT, variables.camera.near, variables.camera.far);
+  const depth = variables.camera.initial.position.z;
+  const boxWidth = visibleWidthAtZDepth(depth, camera);
+  const boxHeight = visibleHeightAtZDepth(depth, camera); 
+  console.debug(`WIDTH/HEIGHT: ${WIDTH}/${HEIGHT}`);
+  var scene = new THREE.Scene();
+  console.debug(`Width/Height: ${boxWidth}/${boxHeight} at visible at depth ${depth}`);
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.position.z = depth;
+  controls = new OrbitControls(camera); 
+  controls.target.set(0, 0, 0)
+
+  // Set properties and object variables. 
+  let properties = new Map();
+  const indexToObject = new Map();
+
+
+  const setInitialProperties = () => {
+    for (let key = 0; key < variables.spheres.number; key++) {
+      if (!properties.has(key)) {
+        properties.set(key, {});
+      }
+      const property = properties.get(key);
+      set(property, 'velocity.x', getRandom() * variables.spheres.maxSpeed);
+      set(property, 'velocity.y', getRandom() * variables.spheres.maxSpeed);
+      set(property, 'velocity.z', getRandom() * variables.spheres.maxSpeed);
+      set(property, 'position.x', getRandom() * variables.box.widthFactor * boxWidth);
+      set(property, 'position.y', getRandom() * variables.box.heightFactor * boxHeight);
+      set(property, 'position.z', getRandom() * variables.box.depth);
+      properties.set(key, property);
     }
-    const property = properties.get(key);
-    set(property, 'velocity.x', getRandom() * variables.spheres.maxSpeed);
-    set(property, 'velocity.y', getRandom() * variables.spheres.maxSpeed);
-    set(property, 'velocity.z', getRandom() * variables.spheres.maxSpeed);
-    set(property, 'position.x',getRandom()*variables.box.widthFactor*boxWidth);
-    set(property, 'position.y',getRandom()*variables.box.heightFactor*boxHeight);
-    set(property, 'position.z',getRandom()*variables.box.depth);
-    properties.set(key, property);
   }
-  return properties
-}
 
-function getRandom() {
-  return -(Math.random() - 0.5);
-}
+  const getProperties = () => {
+    if(properties.size > 0){
+      return properties;
+    } else {
+      setInitialProperties();
+      return properties;
+    }
+  }
+  const updateVelocity = (key, mesh) => {
+    const x = mesh.position.x + getSafe(key, 'velocity.x');
+    const y = mesh.position.y + getSafe(key, 'velocity.y');
+    const z = mesh.position.z + getSafe(key, 'velocity.z');
+    mesh.position.set(x, y, z);
+  }
 
-export {visibleHeightAtZDepth, visibleWidthAtZDepth, getProperties}
+  const updateWall = (key, mesh) => {
+    let vx = getSafe(key, 'velocity.x');
+    let vy = getSafe(key, 'velocity.y');
+    let vz = getSafe(key, 'velocity.z');
+    if (Math.abs(mesh.position.x) >= variables.box.widthFactor * boxWidth) {
+      vx = -1 * vx
+      setSafe(key, 'velocity.x', vx);
+    }
+    if (Math.abs(mesh.position.y) >= variables.box.heightFactor * boxHeight) {
+      vy = -1 * vy
+      setSafe(key, 'velocity.y', vy);
+    }
+    if (Math.abs(mesh.position.z) >= variables.box.depth) {
+      vz = -1 * vz
+      setSafe(key, 'velocity.z', vz);
+    }
+  }
+  const getSafe = (key, prop) => {
+    return get(properties.get(key), prop, get(variables.spheres.initial, prop));
+  }
+
+  const setSafe = (key, prop, value) => {
+    set(properties.get(key), prop, value);
+  }
+  const stepInTimeObject = (key, mesh) => {
+    updateWall(key,mesh);
+    updateVelocity(key,mesh);
+  }
+
+  const setObjects = () => {
+    for (let i = 0; i < variables.spheres.number; i++) {
+      const colors = variables.spheres.colors;
+      var color = colors[Math.floor(Math.random() * colors.length)];
+      const material = new THREE.MeshBasicMaterial({
+        color: color
+      });
+      const sphereGeometry = new THREE.SphereGeometry(
+        variables.spheres.initial.radius,
+        variables.spheres.initial.widthSegments,
+        variables.spheres.initial.heightSegments,
+        variables.spheres.initial.phiStart,
+        variables.spheres.initial.phiLength,
+        variables.spheres.initial.thetaStart,
+        variables.spheres.initial.thetaLength
+      );
+      const sphereMesh = new THREE.Mesh(sphereGeometry, material)
+      scene.add(sphereMesh);
+      indexToObject.set(i, sphereMesh);
+      indexToObject.get(i).position.set(getSafe(i, 'position.x'), getSafe(i, 'position.y'), getSafe(i, 'position.z'));
+    }
+  }
+
+  const getRandom = () => {
+    return -(Math.random() - 0.5);
+  }
+  setInitialProperties();
+  setObjects();
+  const update = () => {
+    for (let key of indexToObject.keys()) {
+      const mesh = indexToObject.get(key);
+      stepInTimeObject(key, mesh)
+    }
+  }
+  return {
+    update,
+    scene,
+    camera,
+    renderer
+  }
+}
