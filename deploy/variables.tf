b0e25d996280cac720d5a705f2d42b9255a7c53c
Alexander Morton <mortonprod@gmail.com>
2019-01-25 17:27:24 +0000

    Add: Deployment example

---
+++ deploy/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  default = "wgl-site"
+}
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "domain_names" {
+  description = "Only supports sub domain changes"
+  default = ["www.alex-test-site.co.uk", "test.alex-test-site.co.uk"]
+}
+
+variable "asset_folder" {
+  description = "Only supports sub domain changes"
+  default = "./website/dist"
+}
