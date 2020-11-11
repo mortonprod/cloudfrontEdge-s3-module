2b7f57468f34638be7aa6c981cf840af55dcfe98
Alexander Morton <mortonprod@gmail.com>
2020-11-11 00:35:55 +0000

    Remove comment out code. Update response to redirect on error

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,19 +1,16 @@
 const path = require('path');
 const extracts = [/\/[a-zA-Z0-9_.-]*\/[a-zA-Z0-9_.-]*/,/\/[a-zA-Z0-9_.-]*/];
 /**
- * This part will redirect the client to the correct index file.
+ * This part will redirect the client to the correct index file for / or /something or /something/something
+ * Note we will only redirect if no extension is present.
  */
 exports.originRequest = (event, context, callback) => {
   const { request } = event.Records[0].cf;
-  
-//   console.log('Request URI: ', request.uri);
   /**
    * Use node to parse the input request url to determine if we need to append and index.html to it. 
    */
   const parsedPath = path.parse(request.uri);
   let newUri;
-
-//   console.log('Parsed Path: ', parsedPath);
   /**
    * If there is an extension then we just need to served the static assets at that location
    * Otherwise route to the index.html file
@@ -23,13 +20,11 @@ exports.originRequest = (event, context, callback) => {
     /**
      * If no extension found then we need to find the index.html file
      * Loop through all regex until we get a match.
+     * TODO: Should be able to do this without a loop but will do for now.
      */
     for (let extract of extracts) {
         let match = request.uri.match(extract);
-        // console.log(`Match ${match} ${request.uri} ${extract}`);
         if(match) {
-            // console.log(`Match Inside ${match[0]} ${typeof match}`);
-            // newUri = path.join(match[0], 'index.html');
             newUri = match[0] + '/' + 'index.html';
             isMatched = true;
             break
@@ -45,8 +40,6 @@ exports.originRequest = (event, context, callback) => {
     newUri = request.uri;
   }
 
-//   console.log('New URI: ', newUri);
-
   // Replace the received URI with the URI that includes the index page
   request.uri = newUri;
   
@@ -55,45 +48,52 @@ exports.originRequest = (event, context, callback) => {
 };
 /**
  * This will add the correct security headers to the response.
- * Can't do this with WAF rules it seems.
  */
 exports.originResponse = (event, context, callback) => {
 
     const response = event.Records[0].cf.response;
-    
-    const headers = response.headers;
-    
-    headers["x-frame-options"] = [{
+    /**
+     * If we get an error from the origin assume we could not find the html page and return to index.
+     * Otherwise update with security headers.
+     */
+    if (response.status >= 400 && response.status <= 599) {
+      const redirect_path = `/index.html`;
+
+      response.status = 302;
+      response.statusDescription = 'Found';
+
+      /* Drop the body, as it is not required for redirects */
+      response.body = '';
+      response.headers['location'] = [{ key: 'Location', value: redirect_path }];
+    } else {
+
+      response.headers["x-frame-options"] = [{
         key: "x-frame-options",
         value: "SAME-ORIGIN",
-    }];
+      }];
 
-    headers["strict-transport-security"] = [{
-        key: "strict-transport-security",
-        value: "max-age=15552000; includeSubDomains",
-    }];
+      response.headers["strict-transport-security"] = [{
+          key: "strict-transport-security",
+          value: "max-age=15552000; includeSubDomains",
+      }];
 
-    headers["x-content-type-options"] = [{
-        key: "x-content-type-options",
-        value: "nosniff",
-    }];
-    headers["x-xss-protection"] = [{
-        key: "x-xss-protection",
-        value: "1; mode=block",
-    }];
-    headers["content-security-policy"] = [{
-        key: "content-security-policy",
-        value: "default-src 'self'; connect-src 'self' ses.alexandermorton.co.uk; script-src 'self' cdnjs.cloudflare.com code.getmdl.io code.jquery.com; style-src 'self' code.getmdl.io fonts.googleapis.com; font-src 'self' code.getmdl.io fonts.googleapis.com fonts.gstatic.com",
-    }];
-    headers["Referrer-Policy"] = [{
-        key: "Referrer-Policy",
-        value: "same-origin",
-    }];
-    headers["Cache-Control"] = [{
-      key: "Cache-Control",
-      value: "max-age=31536000",
-  }];
-    
+      response.headers["x-content-type-options"] = [{
+          key: "x-content-type-options",
+          value: "nosniff",
+      }];
+      response.headers["x-xss-protection"] = [{
+          key: "x-xss-protection",
+          value: "1; mode=block",
+      }];
+      response.headers["content-security-policy"] = [{
+          key: "content-security-policy",
+          value: "default-src 'self'; connect-src 'self' ses.alexandermorton.co.uk; script-src 'self' cdnjs.cloudflare.com code.getmdl.io code.jquery.com; style-src 'self' code.getmdl.io fonts.googleapis.com; font-src 'self' code.getmdl.io fonts.googleapis.com fonts.gstatic.com",
+      }];
+      response.headers["Referrer-Policy"] = [{
+          key: "Referrer-Policy",
+          value: "same-origin",
+      }];
+    } 
     callback(null, response);
     
 };
